27da1f6ae76afa28c2f8238648688ad7ecfb7684
Divyanshu Singh <your-divyanshu.hb10@gmail.com>
2026-07-26 01:15:50 +0530

    Completed Angular Hands-on-1

---
+++ student-course-portal/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { Header } from './header/header';
+import { Home } from './home/home';
+
+@Component({
+  selector: 'app-root',
+  imports: [Header, Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('student-course-portal');
+}
