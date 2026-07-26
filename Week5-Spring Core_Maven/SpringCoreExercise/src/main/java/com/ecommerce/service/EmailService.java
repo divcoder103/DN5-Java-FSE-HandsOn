e15b2e696318fcbc5b2f33a15a1e98e7895b0591
Divyanshu Singh <your-divyanshu.hb10@gmail.com>
2026-07-26 21:37:51 +0530

    Remove duplicate folders and clean repository

--- Week5-Spring Core_Maven/SpringCoreExercise/src/main/java/com/ecommerce/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.service;
-
-public class EmailService {
-
-    public void sendEmail() {
-        System.out.println("Email Sent Successfully");
-    }
-
-}
--- Week5-Spring Core_Maven/SpringCoreExercise/src/main/java/com/ecommerce/service/GreetingServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.service;
-
-public class GreetingServiceImpl implements GreetingService {
-
-    @Override
-    public void greet(){
-        System.out.println("Welcome to Spring Framework");
-    }
-}
